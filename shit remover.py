d6e9261d85decea28865f1a174a94a75b06dc7cd
Pseudogenesis <samattlevine@yahoo.com>
2015-10-10 20:20:22 -0700

    Shit remover 1.0
    First working version, added a description and updated comments.

--- shit remover.py
+++ shit remover.py
@@ -1,8 +1,10 @@
-""" A proof of concept showing how a certain person would choose to
-access and write to files. I honestly still don't know how this works.
-Written by https://github.com/andrensegura """ 
+""" A simple script to remove all non-alphanumeric characters in text files with some readable text 
+and LOTS of random characters. Works decently but doesn't catch everything. 
+Encoding is set to latin-1 because otherwise IDLE throws a "UnicodeDecodeError". """
 
-import os
-f = os.open( "p.txt", os.O_RDWR) #Create the file 
-os.write(f, b"This is a test \n")  #Try to write 
-os.close(f)
+import re # Regular expressions package
+origin = open(r"C:\Users\Sam\Desktop\Quarantine\origin.dat", "r+", encoding="latin-1")       # The "r" before the directory makes it
+subtitles = open(r"C:\Users\Sam\Desktop\Quarantine\subtitles.dat", "r+", encoding="latin-1") # a raw string, elminating the need for escape chars. 
+subtitles.write(re.sub('[^a-zA-Z0-9_\s:]', '',origin.read())) # I have no goddamn idea how that regex works, just took it from stackoverflow                              
+origin.close()
+subtitles.close()
