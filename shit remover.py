781ccba70fa1c860076e1a5d49051411c7f36375
Pseudogenesis <samattlevine@yahoo.com>
2015-10-10 20:09:21 -0700

    Read/write proof of concept
    Added description

--- shit remover.py
+++ shit remover.py
@@ -1,3 +1,7 @@
+""" A proof of concept showing how a certain person would choose to
+access and write to files. I honestly still don't know how this works.
+Written by https://github.com/andrensegura """ 
+
 import os
 f = os.open( "p.txt", os.O_RDWR) #Create the file 
 os.write(f, b"This is a test \n")  #Try to write 
