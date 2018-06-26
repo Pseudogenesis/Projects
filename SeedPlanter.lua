80118d822f4dbc8faa7822bc0322bc1ad8977c3c
Pseudogenesis <samattlevine@yahoo.com>
2018-06-25 19:05:27 -0700

    SeedPlanter.lua
    Version 1.5.0 - Extensive optimization improvements, main function refactor, and other organizational tweaks

--- SeedPlanter.lua
+++ SeedPlanter.lua
@@ -1,20 +1,20 @@
--- This is Seed Planter, a seed log generator for The Binding of Isaac: Afterbirth+
--- This is pretty much my 3rd real programming project ever, so any bug reports, pull requests and constructive feedback are always appreciated. 
--- Feel free to message my reddit account /u/Pseudogenesis at any time, or to submit an issue or pull request to Github: https://github.com/Pseudogenesis
--- I'm honestly not super motivated to return to this project, but if you have a major bug to report or a relatively simple feature to request, I might give it a shot. 
--- I'd still like constructive criticism though, even if I might not carry it out. 
--- Special thanks to Taiga, budj and bibbleskit for their indispensible help in figuring out how to put this monstrosity together.
+--[[ This is Seed Planter, a seed log generator for The Binding of Isaac: Afterbirth+
+	 This is pretty much my 3rd real programming project ever, so any bug reports, pull requests and constructive feedback are always appreciated. 
+	 Feel free to message my reddit account /u/Pseudogenesis at any time, or to submit an issue or pull request to Github: https://github.com/Pseudogenesis
+	 I'm honestly not super motivated to return to this project, but if you have a major bug to report or a relatively simple feature to request, I might give it a shot. 
+	 I'd still like constructive criticism though, even if I might not carry it out. 
+	 Special thanks to Taiga, budj and Andre Segura for their indispensible help in figuring out how to put this monstrosity together. ]]--
 
--- As it stands, this doesn't support record items that have been rerolled or actives that have been replaced. Each item list is created anew whenever a boss is killed. 
--- I spent half a day trying to fix it, but due to the way the program is structured it was much more difficult for me than I thought. I might fix it eventually but it'd likely require a major refactoring that I'm not motivated to work on
+--[[ As it stands, this doesn't support record items that have been rerolled or actives that have been replaced. Each item list is created anew whenever a boss is killed. 
+	 I spent half a day trying to fix it, but due to the way the program is structured it was much more difficult for me than I thought. I might fix it eventually but it'd likely require a major refactoring that I'm not motivated to work on ]]--
 
 -- STATIC VARIABLES
-local seedMod = RegisterMod("Seed Planter 1.0", 1)
+local seedMod = RegisterMod("Seed Planter 1.5", 1)
 
--- Notable items the run might've had. Based on the top 30 items on IsaacRanks.com as of 5/27/18, game-designated special items, and any other items I thought were run-defining, powerful, notable or memorable
--- Some items, like Guppy's Collar, Rotten Baby and Gnawed Leaf, don't necessarily fit these criteria but are included for the sake of identifying possible transformations or interesting synergies (Guppy, fly synergy, etc) 
--- If you want to add items to this list yourself, it's fairly simple. The format is [item ID] = "Item Name", with each entry separated by a comma.
--- The Item ID can be found on PlatinumGod.co.uk or the Isaac wiki, that goes in brackets. I'm not entirely sure how one might add items that have been modded in, but it's probably possible.  
+--[[ Notable items the run might've had. Based on the top 30 items on IsaacRanks.com (as of 5/27/18), game-designated special items, and any other items I thought were run-defining, powerful, notable or memorable
+	 Some items, like Guppy's Collar, Rotten Baby and Gnawed Leaf, don't necessarily fit these criteria but are included for the sake of identifying possible transformations or interesting synergies (Guppy, fly synergy, etc) 
+	 If you want to add items to this list yourself, it's fairly simple. The format is [item ID] = "Item Name", with each entry separated by a comma.
+	 The Item ID can be found on PlatinumGod.co.uk or the Isaac wiki, that goes in brackets. I'm not entirely sure how one might add items that have been modded in, but it's probably possible. ]]-- 
 local NotableItemsDict = {[12] = "Magic Mushroom", [245] = "20/20",[182] = "Sacred Heart",[313] = "Holy Mantle",[395] = "Tech X",[118] = "Brimstone",[153] = "Mutant Spider",[399] = "Maw of the Void", [11] = "1up!",
 				[331] = "Godhead",[105] = "D6",[237] = "Death's Touch",[4] = "Cricket's Head",[169] = "Polyphemus",[80] = "The Pact",[108] = "The Wafer",[360] = "Incubus",[230] = "Abaddon",[261] = "Proptosis",
 				[494] = "Jacob's Ladder",[114] = "Mom's Knife",[415] = "Crown of Light",[223] = "Pyromaniac",[307] = "Capricorn",[224] = "Cricket's Body",[345] = "Synthoil",[101] = "The Halo",[168] = "Epic Fetus",
@@ -34,10 +34,11 @@ local NotableItemsDict = {[12] = "Magic Mushroom", [245] = "20/20",[182] = "Sacr
 				[496] = "Euthanasia", [506] = "Backstabber", [522] = "Telekinesis", [523] = "Moving Box", [526] = "7 Seals", [530] = "Death's List", [532] = "Lachryphagy", [531] = "Haemolacria", [533] = "Trisagion"}
 				
 
-local json = require("json") -- AB+ includes JSON4Lua, a JSON encoding utility. This mod uses it to store and retrieve info from the save file as tables. 
-							 -- Before I realized AB+ supported JSON, I tried to work with just string manipulations. It was a nightmare, do not recommend
+local json = require("json") --[[AB+ includes JSON4Lua, a JSON encoding utility. This mod uses it to store and retrieve info from the save file as tables. 
+								 Before I realized AB+ supported JSON, I tried to work with just string manipulations. It was a nightmare, do not recommend ]]--
 
 local difficultyTable = {[0] = "Normal", [1] = "Hard", [2] = "Greed", [3] = "Greedier"}
+
 
 -- DYNAMIC VARIABLES
 
@@ -48,13 +49,13 @@ local firstTimeUser
 local gameStartCheckDone = false
 local currentItems = ""
 
--- FUNCTIONS
 
+-- FUNCTIONS
 
 function seedMod:GameStartCheck(fromSaveGame)
 	gameStartCheckDone = true
 	if fromSaveGame and (Game():GetVictoryLap() == 0) then -- If the game is being started from a save file
-		currentRunDataExists = true -- This flag tells the mod to edit the current continued run's data instead of creating a whole new seed entry
+		currentRunDataExists = true -- Tells the mod to edit the current continued run's data instead of creating a whole new seed entry
 		trackingItems = 1
 		seedMod:UpdateItemsList()
 	elseif (not fromSaveGame) and (Game():GetVictoryLap() == 0) then -- If the run is brand new
@@ -66,77 +67,110 @@ function seedMod:GameStartCheck(fromSaveGame)
 	end
 end
 
+function seedMod:CustomEncode(tbl) -- Bundles json.encode() with our own pretty printing functions
+	local encodedSeed = json.encode(tbl)
+	encodedSeed = seedMod:make_pretty(encodedSeed) -- Pretty printer ensures the seed data is easily readable by ordinary users
+	encodedSeed = seedMod:ExtraPretty(encodedSeed) -- Even more pretty	
+	return encodedSeed
+end
+
+function seedMod:CustomDecode(str) -- Same as above but for decoding
+	local decodedSeed = seedMod:ReverseExtraPretty(str)
+	decodedSeed = json.decode(decodedSeed)
+	return decodedSeed
+end
 
 
-function seedMod:SaveInfo()
+
+function seedMod:IsolateSeeds(data) -- Takes the log data, extracts the most recent seed info, and separates it all into distinct parts. masterTable contains everything, lastSeedTable is just lastSeed after JSON4Lua is through with it. 
+	local masterTable = {}
+	local lastSeedTable = {}
+	local lastSeed = ""
+	table.insert(lastSeedTable, string.match(data, "{.*}"))
+	lastSeed = table.concat(lastSeedTable)
+	Isaac.ConsoleOutput(lastSeed)
+	lastSeedTable = seedMod:CustomDecode(lastSeed)
+	table.insert(masterTable, lastSeed)
+	table.insert(masterTable, string.match(data, "}(.*)"))
+	return lastSeed, lastSeedTable, masterTable
+end
+
+function seedMod:SaveInfo() -- The main logical structure of the program. If there was ever something I was going to refactor, it'd be this. Although it's *much* cleaner now that I moved several major code blocks into their own functions. 
 	if Game():GetVictoryLap() == 0 then
+		local lastSeed = ""
+		local lastSeedTable, masterTable = {}
+		
 		trackingItems = 1
 		seedData = ""
+		local seedDataTableBuffer = {}
+		
 		local player = Isaac.GetPlayer(0)
 		local seed = Game():GetSeeds():GetStartSeedString() 
 		local name = player:GetName()
 		local mode = difficultyTable[Game().Difficulty]
-		local date = os.date("%x %X")
-		local currentTransformations = seedMod:RecordTransformations()
-		currentTransformations = table.concat(currentTransformations)
+		-- local datetime = os.date("%x %X") -- Disabled due to the os library being inaccessible through the API under normal circumstances. No way around this, unfortunately. I plan to replace Date with a measure of run length. 
+		local datetime = "Disabled - impossible to access with API"
+		local transforms = seedMod:RecordTransformations()
 		local isChallenge = Game().Challenge -- All challenges are Normal difficulty but do not support starting from seed. I'm supporting challenges anyway, just in case somebody really likes the run the seed generated. 
 		if isChallenge > 0 then
 			mode = "Challenge (Normal)"
 		end
+		
 		if currentItems == "" then
 			currentItems = "No notable items"
 		end
-		date = string.gsub(date, "/", "-") -- "/"s gets turned into "\/" after being parsed by JSON4Lua, which looks really weird. "-"s circumvent this
+		
+		local currentSeedInfo = {Date = datetime, Items = currentItems, Seed = seed, Name = name, Mode = mode, Transformations = transforms}
+		-- datetime = string.gsub(datetime, "/", "-") -- "/"s gets turned into "\/" after being parsed by JSON4Lua, which looks really weird. "-"s circumvent this
 		seedData = Isaac.LoadModData(seedMod)
-		seedData = seedMod:ReverseExtraPretty(seedData)
+		
 		if string.len(seedData) == 0 then -- If the save file is completely empty, i.e. somebody just installed the mod
 			firstTimeUser = true
-			seedData = {[1] = {Date = date, Items = currentItems, Seed = seed, Name = name, Mode = mode, Transformations = currentTransformations}}
-			seedData = json.encode(seedData)
-			seedData = seedMod:make_pretty(seedData) -- Pretty printer that ensures the seed data is easily readable by ordinary users
-			seedData = seedMod:ExtraPretty(seedData) -- Even more pretty
+			currentSeedInfo = seedMod:CustomEncode(currentSeedInfo)
+			seedData = currentSeedInfo
 			Isaac.SaveModData(seedMod, seedData)
 			currentRunDataExists = true
 		else
-			seedData = json.decode(seedData)
+			lastSeed, lastSeedTable, masterTable = seedMod:IsolateSeeds(seedData)
 		end
 		if (not currentRunDataExists) and (not firstTimeUser) then -- If it's a new run, append a new table with the current run's data to the master table
-			table.insert(seedData, 1, {Date = date, Items = currentItems, Seed = seed, Name = name, Mode = mode, Transformations = currentTransformations})
+			table.insert(masterTable, 1, currentSeedInfo)
+			masterTable[1] = seedMod:CustomEncode(masterTable[1])
 			currentRunDataExists = true
 		elseif currentRunDataExists and (not firstTimeUser) then -- If it's a continued run, then just edit the existing items list.
-
-			if string.len(seedData[1].Items) < string.len(currentItems) then -- Comparing lengths prevents things like complete item rerolls from deleting the existing item data. Old will be overwritten if the new notable items list exceeds it in length
-				seedData[1].Items = currentItems
-				seedData[1].Transformations = currentTransformations
-			elseif seedData[1].Items == "No notable items" and currentItems ~= "No notable items" then -- Ensures that short currentItems lists, like "20-20, Tech 2" would overwrite the "No notable items" string. 
-				seedData[1].Items = currentItems
-				seedData[1].Transformations = currentTransformations
+			masterTable[1] = lastSeedTable
+			if string.len(masterTable[1].Items) < string.len(currentItems) then -- Comparing lengths prevents things like complete item rerolls from deleting the existing item data. Old will be overwritten if the new notable items list exceeds it in length
+				masterTable[1] = currentSeedInfo
+			elseif masterTable[1].Items == "No notable items" and currentItems ~= "No notable items" then -- Ensures that short currentItems lists, like "20-20, Tech 2" would overwrite the "No notable items" string. 
+				masterTable[1] = currentSeedInfo
 			else
-				seedData[1].Transformations = currentTransformations
+				masterTable[1].Transformations = transforms
+				-- Run length will also be set here, when I implement it
 			end
+			masterTable[1] = seedMod:CustomEncode(masterTable[1])
 		end
+		
 		if (not firstTimeUser) then -- We only save the data if they're not a first time user. If they are (ie have just installed the mod) then their data has already been saved at this point.
-			seedData = json.encode(seedData)
-			seedData = seedMod:make_pretty(seedData)
-			seedData = seedMod:ExtraPretty(seedData)
+			table.insert(masterTable, 1, "SEEDS ARE IN CHRONOLOGICAL ORDER. The top seed is the newest, the bottom seed is the oldest. Enjoy!\n") -- This message stays at the top every time because of the way IsolateSeeds() handles reading the log. 
+			seedData = table.concat(masterTable)
 			Isaac.SaveModData(seedMod, seedData)
 		else
 			firstTimeUser = false
 		end
+		
 	else
 		trackingItems = 0 -- VICTORY LAPS MUST DIE
 	end
 end
 
 
-function seedMod:UpdateItemsList() -- Cycles through our list of notable items and adds them to a currentItems list for later. Calls SaveInfo() at the end. 
+function seedMod:UpdateItemsList(fromBossKill) -- Cycles through our list of notable items and adds them to a currentItems list for later. Calls SaveInfo() at the end. 
 	if gameStartCheckDone == true then
 		local player = Isaac.GetPlayer(0)
 		local numberOfItems = 0
 		itemTableBuffer = {}
 		currentItems = ""
 		local seedData = Isaac.LoadModData(seedMod)
-		seedData = seedMod:ReverseExtraPretty(seedData)
 		for ItemID,ItemName in pairs(NotableItemsDict) do
 			if (player:HasCollectible(ItemID)) then
 				numberOfItems = numberOfItems + 1
@@ -150,7 +184,9 @@ function seedMod:UpdateItemsList() -- Cycles through our list of notable items a
 		end
 		currentItems = table.concat(itemTableBuffer)
 		currentItems = string.gsub(currentItems, "/", "-") -- Same deal as with date, the item "20/20" is renamed to "20-20" for formatting reasons
-		seedMod:SaveInfo(currentItems)
+		if not fromBossKill then
+			seedMod:SaveInfo(currentItems)
+		end
 	end
 end
 
@@ -171,17 +207,22 @@ function seedMod:RecordTransformations() -- Checks for transformations the playe
 		end
 	end
 	if transformationCount == 0 then
-		local emptyItemsTable = {"None"} -- If we don't make this a table, table.concat() will yell at us
-		return emptyItemsTable
+		currentTransformations = "None" 
+		return currentTransformations
 	else
+		currentTransformations = table.concat(currentTransformations)
 		return currentTransformations
 	end
 end
 			
-function seedMod:CheckDeadNPC(DeadNPC) -- We're only really updating when a boss NPC is killed. This minimizes performance impact and ensures that items are totally saved when an endgame boss like Mega Satan is killed. 
+function seedMod:CheckDeadNPC(DeadNPC) -- CheckDeadNPC calls UpdateItemsList() whenever a boss is killed
 	if DeadNPC:IsBoss() then
 		if ((Game():GetVictoryLap() == 0) and (trackingItems == 1)) then
-			seedMod:UpdateItemsList()
+			if DeadNPC.Type == EntityType.ENTITY_THE_LAMB and Game():GetLevel():GetName() == "Dark Room" then -- CheckDeadNPC only saves info after killing The Lamb, in case the player does a Victory Lap, which doesn't trigger the PRE_GAME_EXIT callback. 
+				seedMod:UpdateItemsList(false)
+			else
+				seedMod:UpdateItemsList(true) -- true means UpdateItemsList() will not call SaveInfo() at the end, so that killing multiple bosses at once doesn't cause performance issues (this was easily visible when killing segmented bosses like Pin)
+			end
 		else
 			trackingItems = 0
 		end
@@ -189,7 +230,7 @@ function seedMod:CheckDeadNPC(DeadNPC) -- We're only really updating when a boss
 end
 
 
-function seedMod:make_pretty(uglystr) -- Pretty printer written by the legendary bibbleskit - https://github.com/andrensegura/ . I have no clue how this voodoo magic works, so comments will be limited to the things I changed. 
+function seedMod:make_pretty(uglystr) -- Pretty printer written by the legendary Andre Segura - https://github.com/andrensegura/ . I have no clue how this voodoo magic works, so comments will be limited to the things I changed. 
     -- Takes an ugly string, returns a prettier one.
     -- Change spaces to your desired indentation width.
 	local tableBuffer = {}
@@ -264,6 +305,33 @@ end
 
 -- CALLBACKS
 
+
+
 seedMod:AddCallback(ModCallbacks.MC_POST_GAME_STARTED, seedMod.GameStartCheck) -- Triggers GameStartCheck upon entering/re-entering a run
 seedMod:AddCallback(ModCallbacks.MC_POST_NPC_DEATH, seedMod.CheckDeadNPC) -- Triggers CheckDeadNPC whenever an NPC is killed
 seedMod:AddCallback(ModCallbacks.MC_PRE_GAME_EXIT, seedMod.OnMenuExit) -- Triggers OnMenuExit when, you guessed it, the player exits to the main menu
+
+-- DEBUG STUFF
+
+-- function seedMod:TestTable(location)  -- Deprecated for now
+	-- Isaac.ConsoleOutput(location .. ": ")
+	-- for k,v in ipairs(masterTable) do
+	-- Isaac.ConsoleOutput(count)
+	-- count = count + 1
+	-- Isaac.ConsoleOutput(tostring(v))
+	-- Isaac.ConsoleOutput("\n")
+	-- end
+-- end
+
+-- Isaac.ConsoleOutput("After master table made: ") - Block for testing contents of the masterTable. 
+-- local count = 1
+-- for k,v in ipairs(masterTable) do
+	-- Isaac.ConsoleOutput(count)
+	-- count = count + 1
+	-- Isaac.ConsoleOutput(tostring(v))
+	-- Isaac.ConsoleOutput("\n")
+-- end
+
+-- function seedMod:Trim(str) -- Removes whitespace. Debug, deprecated for now
+  -- return (str:gsub("^%s*(.-)%s*$", "%1"))
+-- end
